40d7693fe8142d36ea540944aeb7190f23e01e93
Henrik Lundgren <upptaget@gmail.com>
2020-10-09 14:51:58 +0200

    chore: fix scripts

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+
+module.exports = {
+  exportPathMap: () => ({
+    '/': { page: '/' },
+  }),
+  assetPrefix: isProd ? '/sample-frontend-react' : '',
+};
+
